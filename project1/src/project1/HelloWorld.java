ce57a3cc366d963758b40c57a32aef9ac50b47b2
24jy0127 Narazaki shuya <24jy0127@jec.ac.jp>
2025-10-16 14:35:18 +0900

    4回こんにちは世界

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -3,7 +3,8 @@ package project1;
 public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
+		for(int i=0;i<3;i++) {
+			System.out.println("HelloWorld");
+		}
 	}
 }
